6af21532f990b7f748ec6da99e380360532ef121
Deepa Venkatesh <deepa4lok@yahoo.com>
2023-05-09 17:37:39 +0530

    0.3

--- website_event_exhibitors/__manifest__.py
+++ website_event_exhibitors/__manifest__.py
@@ -6,7 +6,7 @@
     'name': 'Event Exhibitors Registration',
     'category': 'Marketing/Events',
     'sequence': 1005,
-    'version': '14.0.2.0',
+    'version': '14.0.3.0',
     'summary': 'Event: upgrade sponsors to exhibitors with registration',
     'author' : 'Deepa, The Open Source Company (TOSC)',
     'website': 'https://www.tosc.nl',
