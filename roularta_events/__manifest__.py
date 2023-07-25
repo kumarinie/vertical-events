2c514d28eee9ce894a0f40c7663071bd0eaeb80d
Deepa Venkatesh <deepa4lok@yahoo.com>
2023-07-25 13:04:41 +0530

    Roularta Events: Initial Commit

---
+++ roularta_events/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Roularta Events',
+    'category': 'Marketing/Events',
+    'sequence': 1005,
+    'version': '14.0.0',
+    'summary': 'Event: Improvements specific to Roularta',
+    'author' : 'Deepa, The Open Source Company (TOSC)',
+    'website': 'https://www.tosc.nl',
+    'description': "",
+    'depends': [
+        'website_event_exhibitors'
+    ],
+    'data': [
+        "views/sale_report_template.xml",
+        "views/invoice_report_template.xml",
+    ],
+    'demo': [
+    ],
+    'application': False,
+    'installable': True,
+    'license': 'LGPL-3',
+}
