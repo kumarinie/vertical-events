228a4a4ba16afdc07e61d8377294972dea28eef2
Deepa Venkatesh <deepa4lok@yahoo.com>
2023-08-07 10:36:55 +0530

    Exhibitor 14.0.12; Roularta 14.0.2

--- roularta_events/__manifest__.py
+++ roularta_events/__manifest__.py
@@ -6,7 +6,7 @@
     'name': 'Roularta Events',
     'category': 'Marketing/Events',
     'sequence': 1005,
-    'version': '14.1.0',
+    'version': '14.0.2',
     'summary': 'Event: Improvements specific to Roularta',
     'author' : 'Deepa, The Open Source Company (TOSC)',
     'website': 'https://www.tosc.nl',
