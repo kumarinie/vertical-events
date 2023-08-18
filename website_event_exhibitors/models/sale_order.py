51d9338c9c107fbded6fcf3298028a24c005cb45
Deepa Venkatesh <deepa4lok@yahoo.com>
2023-08-18 18:46:35 +0530

    Exhibitor: Force Domain

--- website_event_exhibitors/models/sale_order.py
+++ website_event_exhibitors/models/sale_order.py
@@ -12,3 +12,21 @@ class SaleOrder(models.Model):
     event_id = fields.Many2one('event.event', string='Event')
 
 
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    @api.model
+    def _get_domain4event_products(self):
+        # Force Domain
+
+        return "[('sale_ok', '=', True), '|', ('company_id', '=', False), ('company_id', '=', parent.company_id), \
+                    '|', ('website_id', '=', False), ('website_id', '=', parent.website_id)]"
+
+
+    # Inherited
+    product_id = fields.Many2one('product.product', domain=_get_domain4event_products)
+
+
+
+
