0fa893c13694d2e9544472f1b0048cc7705ef08a
Deepa Venkatesh <deepa4lok@yahoo.com>
2024-08-28 16:37:44 +0530

    Event: Contact person

--- website_event_exhibitors/models/sale_order.py
+++ website_event_exhibitors/models/sale_order.py
@@ -11,6 +11,7 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     event_id = fields.Many2one('event.event', string='Event', ondelete='restrict', tracking=True)
+    customer_contact = fields.Many2one('res.partner', 'Contact Person', domain=[('is_customer', '=', True)])
 
     @api.onchange('brand_id')
     def _onchange_brand(self):
@@ -53,6 +54,26 @@ class SaleOrder(models.Model):
             for line in self.order_line:
                 line.product_id_change()
 
+    @api.onchange('partner_id')
+    def onchange_partner2(self):
+        if not self.partner_id:
+            self.customer_contact = False
+
+        if self.partner_id.type == 'contact':
+            contact = self.env['res.partner'].search([('is_company','=', False),('type','=', 'contact'),('parent_id','=', self.partner_id.id)])
+            if len(contact) >=1:
+                contact_id = contact[0]
+            else:
+                contact_id = False
+        else:
+            addr = self.partner_id.address_get(['delivery', 'invoice'])
+            contact_id = addr['contact']
+        if not self.partner_id.is_company and not self.partner_id.parent_id:
+            contact_id = self.partner_id
+
+        if not self.customer_contact:
+            self.customer_contact = contact_id
+
     @api.model
     def create(self, vals):
         website_id = self.env.context.get('website_id', False)
@@ -71,6 +92,12 @@ class SaleOrder(models.Model):
             vals['event_id'] = event.id
             vals['brand_id'] = event.brand_id and event.brand_id.id or False
         return super(SaleOrder, self).create(vals)
+
+    def _prepare_invoice(self):
+        invoice_vals = super(SaleOrder, self)._prepare_invoice()
+        if self.customer_contact:
+            invoice_vals['customer_contact'] = self.customer_contact.id
+        return invoice_vals
 
 
 
